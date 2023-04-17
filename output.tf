1cf1451cbba319eddefadffd6081d31a4de0699a
yakuramori <62520712+yury-dubinin@users.noreply.github.com>
2023-04-17 12:59:26 +0200

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,9 @@
+output "location" {
+  description = "The Azure region"
+  value       = azurerm_resource_group.sigrg.location
+}
+
+output "Compute_Gallery" {
+  description = "Azure Compute Gallery"
+  value       = azurerm_shared_image_gallery.sig.name
+}
