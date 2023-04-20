600b47979208d93fddb4e9a96d8521f405a98a8c
yury-dubinin <yury.dubinin@gmail.com>
2023-04-20 21:27:06 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,15 +1,21 @@
 terraform {
+  backend "local" {
+    path = "terraform.tfstate"
+  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.0"
+      version = "~> 3.0"
+    }
+    hcp = {
+      source  = "hashicorp/hcp"
+      version = "~> 0.46"
     }
     azuread = {
       source = "hashicorp/azuread"
     }
   }
 }
-
 provider "azurerm" {
   features {}
 }
